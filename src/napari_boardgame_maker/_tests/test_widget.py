d54fc2c7652cc81348eecbe9c793e63626e58e40
Johannes Müller <johannes.mueller88@hotmail.de>
2023-08-13 17:57:04 +0200

    created tests

--- src/napari_boardgame_maker/_tests/test_widget.py
+++ src/napari_boardgame_maker/_tests/test_widget.py
@@ -1,36 +1,22 @@
 import numpy as np
 
-from napari_boardgame_maker import ExampleQWidget, example_magic_widget
 
+def test_widget(make_napari_viewer):
 
-# make_napari_viewer is a pytest fixture that returns a napari viewer object
-# capsys is a pytest fixture that captures stdout and stderr output streams
-def test_example_q_widget(make_napari_viewer, capsys):
-    # make viewer and add an image layer using our fixture
+    from .._sample_data import rhone_glacier
+    from .. import BoardgameMakerWidget
+
+    image = rhone_glacier()[0][0]
     viewer = make_napari_viewer()
-    viewer.add_image(np.random.random((100, 100)))
 
-    # create our widget, passing in the viewer
-    my_widget = ExampleQWidget(viewer)
+    # add the image
+    layer = viewer.add_image(image, name='rhone')
 
-    # call our widget method
-    my_widget._on_click()
+    widget = BoardgameMakerWidget(viewer)
+    viewer.window.add_dock_widget(widget, area='right')
+    #widget.image_layer_select.set_choice(layer)
+    widget._create_outline()
+    widget._create_number_field()
 
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == "napari has 1 layers\n"
-
-
-def test_example_magic_widget(make_napari_viewer, capsys):
-    viewer = make_napari_viewer()
-    layer = viewer.add_image(np.random.random((100, 100)))
-
-    # this time, our widget will be a MagicFactory or FunctionGui instance
-    my_widget = example_magic_widget()
-
-    # if we "call" this object, it'll execute our function
-    my_widget(viewer.layers[0])
-
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == f"you have selected {layer}\n"
+    widget._create_tile()
+    
--- src/napari_boardgame_maker/_tests/test_writer.py
+++
@@ -1,7 +0,0 @@
-# from napari_boardgame_maker import write_single_image, write_multiple
-
-# add your tests here...
-
-
-def test_something():
-    pass
